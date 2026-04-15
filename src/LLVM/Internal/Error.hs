a31397d47444979efb40fdfa490e7b29622f6724
prophet <mail@prophetlabs.de>
2026-04-15 02:21:02 +0200

    Include a call stack in LLVM errors

--- src/LLVM/Internal/Error.hs
+++ src/LLVM/Internal/Error.hs
@@ -6,9 +6,10 @@ import Data.Text qualified as Text
 import Data.Text.Foreign qualified as Text.Foreign
 import Foreign (Ptr, alloca, peek)
 import Foreign.C (CString)
+import GHC.Stack.Types (HasCallStack)
 import LLVM.FFI.Core qualified as Raw
 
-withErrorMessage :: Maybe Text -> (Ptr CString -> IO Raw.Bool) -> IO ()
+withErrorMessage :: (HasCallStack) => Maybe Text -> (Ptr CString -> IO Raw.Bool) -> IO ()
 withErrorMessage context cont = do
     alloca @CString \errorMessagePtr -> mask \restore -> do
         hasErrored <- restore (cont errorMessagePtr)
